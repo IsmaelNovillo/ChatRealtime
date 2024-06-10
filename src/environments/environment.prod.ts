caeaa1d6a3e891a6c9feb2055b0a25dd9cd499f5
NOVILLO ISMAEL <117743714+IsmaelNovillo@users.noreply.github.com>
2024-06-10 11:15:31 -0500

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyD9vtkKDUHGJxEIvVADv0miHPqRXS9wm3E",
+    authDomain: "chat2023-bae0a.firebaseapp.com",
+    databaseURL: "https://chat2023-bae0a-default-rtdb.firebaseio.com",
+    projectId: "chat2023-bae0a",
+    storageBucket: "chat2023-bae0a.appspot.com",
+    messagingSenderId: "578691690025",
+    appId: "1:578691690025:web:e4316e29d5faecddf9b638",
+    measurementId: "G-DPS7004V9M"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,28 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyD9vtkKDUHGJxEIvVADv0miHPqRXS9wm3E",
+    authDomain: "chat2023-bae0a.firebaseapp.com",
+    databaseURL: "https://chat2023-bae0a-default-rtdb.firebaseio.com",
+    projectId: "chat2023-bae0a",
+    storageBucket: "chat2023-bae0a.appspot.com",
+    messagingSenderId: "578691690025",
+    appId: "1:578691690025:web:e4316e29d5faecddf9b638",
+    measurementId: "G-DPS7004V9M"
+  }
+  
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
